95c8180c69318518763b4e2e27b40f4440ebaed1
divyameher <divyaramesh1594@gmail.com>
2021-06-28 16:57:30 +0530

    ion-refresher try

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -7,6 +7,18 @@ import { Component } from '@angular/core';
 })
 export class Tab2Page {
 
-  constructor() {}
-
+  constructor() { }
+  ionRefresh(event) {
+    console.log('ion Refresh event triggered!!');
+    console.log(event.target);
+    event.target.complete();
+  }
+  ionPull(event){
+    console.log('ion pull event triggered!!');
+    // console.log(event.target);
+  }
+  ionStart(event){
+    console.log('ion start event triggered!!');
+    console.log(event.target);
+  }
 }
